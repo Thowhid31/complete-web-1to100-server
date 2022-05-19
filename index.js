6337e1f480d3ddf0eb7e36248bef621190f09812
Thowhid31 <thowhidantor16@gmail.com>
2022-05-19 23:14:46 +0600

    added get mehtod

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+//! Warning: Do not Use in Production
+app.use(
+    cors({origin: "*"})
+)
+
+// app.use(cors());
+app.use(express.json());
+
+
+
+const uri = "mongodb+srv://clean-admin:KlzPKuYVblAdqWnF@cluster0.mugl7.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+      await client.connect();
+      const servicesCollection = client.db('cleanCo').collection('service');
+
+      app.get('/service', async(req, res)=>{
+        const services = await servicesCollection.find({}).toArray();
+        res.send(services)
+    })
+      
+    } finally {
+      
+    }
+  }
+  run().catch(console.dir);
+
 
 app.get('/', (req, res)=>{
     res.send('Hello Server')
