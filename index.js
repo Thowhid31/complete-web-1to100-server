bebf9123ac0496cc0b246fbdded21f377e1d201b
Thowhid31 <thowhidantor16@gmail.com>
2022-05-20 10:39:34 +0600

    added get post put and delete methods

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const cors = require('cors');
+const res = require('express/lib/response');
 const port = process.env.PORT || 5000;
 
 //! Warning: Do not Use in Production
 app.use(
-    cors({origin: "*"})
+  cors({ origin: "*" })
 )
 
 // app.use(cors());
@@ -19,26 +20,77 @@ const uri = "mongodb+srv://clean-admin:KlzPKuYVblAdqWnF@cluster0.mugl7.mongodb.n
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-    try {
-      await client.connect();
-      const servicesCollection = client.db('cleanCo').collection('service');
+  try {
+    await client.connect();
+    const servicesCollection = client.db('cleanCo').collection('service');
 
-      app.get('/service', async(req, res)=>{
-        const services = await servicesCollection.find({}).toArray();
-        res.send(services)
+    app.get('/get-service', async (req, res) => {
+      const services = await servicesCollection.find({}).toArray();
+      res.send(services)
+    });
+
+    app.post('/add-service', async(req, res)=>{
+      const data = req.body;
+      const result  = await servicesCollection.insertOne(data);
+      res.send(result);
+    }) 
+
+    app.put('/update-server/:id', async(req, res)=> {
+      const {id} = req.params;
+      const data = req.body;
+      const filter = {_id: ObjectId(id)}
+      const updateDoc = {$set: data};
+      const option = {upsert: true}
+
+      const result = await servicesCollection.updateOne(filter, updateDoc, option);
+
+      res.send(result);
     })
+
+    app.delete('/delete-server/:id', async(req, res)=> {
+      const {id} = req.params;
       
-    } finally {
-      
-    }
+      const query = {_id: ObjectId(id)}
+     
+
+      const result = await servicesCollection.deleteOne(query);
+
+      res.send(result);
+    })
+
+  } finally {
+
   }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
 
-app.get('/', (req, res)=>{
-    res.send('Hello Server')
+app.get('/', (req, res) => {
+  res.send('Hello Server')
+});
+
+
+//body
+app.get('/dummy-route/user2', (req, res)=>{
+  const data = req.body;
+  res.json(data)
 })
 
-app.listen(port, ()=>{
-    console.log(`Ami Douracchi port ${port}`);
+
+//params
+app.get('/dummy-route/user/:id', (req, res) => {
+  const {id} = req.params;
+  console.log(id);
+    res.json(id)
+});
+
+//query
+app.get('/dummy-route/user', (req, res) => {
+  const data = req.query;
+
+  res.json(data)
+})
+
+app.listen(port, () => {
+  console.log(`Ami Douracchi port ${port}`);
 })
